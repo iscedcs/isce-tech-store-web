564378ae300aea259aaec3bd9b34b34d02017561
Favour <ezehfavour1990@gmail.com>
2026-02-28 04:43:57 -0800

    final form edit for auth

--- components/auth/login/back-button.tsx
+++ components/auth/login/back-button.tsx
@@ -12,7 +12,7 @@ export const BackButton = ({ label1, label2, href }: BackButtonProps) => {
   return (
     <p className="text-sm text-center text-muted-foreground">
       <Link href={href} className="text-blue-600 font-medium hover:underline">
-        <span className="text-black" >{label1}</span> <span className="text-blue-600">{label2}</span>
+        <span className="text-[var(--primary)]" >{label1}</span> <span className="text-[var(--backtext)]">{label2}</span>
       </Link>
     </p>
   );
--- components/auth/login/header.tsx
+++ components/auth/login/header.tsx
@@ -7,15 +7,15 @@ export const Header = ({
     title, subtitle
 }: HeaderProps) => {
     return (
-        <div className="w-ful flex flex-col gap-y-4 items-center justify-center">
+        <div className="w-full flex flex-col items-center justify-center">
             <h1 className="text-3xl font-semibold">
                 <img className="h-10 w-10" src="/image/topicon.jpg" />
             </h1>
-            <h1 className="text-4xl font-bold text-blue-600">
+            <h1 className="text-3xl font-bold bg-gradient-to-r from-[var(--cardheadtext)] to-[var(--cardheadtex)] bg-clip-text text-transparent">
                 {title}
             </h1>
 
-            <p className="text-muted-foreground text-sm">
+            <p className="text-muted-foreground text-[12px]">
                 {subtitle}
             </p>
         </div>
